f21696e66921f0bdedaf93bcf9afc3d44da3f7b5
pazmaciel43-jpg <pazmaciel43@gmail.com>
2026-07-28 00:12:02 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Isso força o uso do diretório 'app'
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = nextConfig
